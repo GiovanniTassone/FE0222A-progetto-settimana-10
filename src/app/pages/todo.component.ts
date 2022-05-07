925fd196a46841ed4fe1741225a99962ff671798
GiovanniTassone <tassonegiovanni33@gmail.com>
2022-05-07 02:27:44 +0200

    Migliorati controlli sulle condizioni delle task

--- src/app/pages/todo.component.ts
+++ src/app/pages/todo.component.ts
@@ -8,14 +8,14 @@ import { aggTask, riceviTask, arrayToDo, ricerca1, addiz, sottr } from "../servi
         <h1>ToDo List</h1>
       </div>
       <div>
-        <input class="form-control w-25 mx-auto" type="text" placeholder="Aggiungi un evento" [(ngModel)]="nomeTask" />
+        <input class="text-capitalize form-control w-25 mx-auto" type="text" placeholder="Aggiungi un evento" [(ngModel)]="nomeTask" />
         <button class="btn btn-info mt-2 border border-primary border-2 fw-bold" (click)="add(nomeTask)">Aggiungi</button>
       </div>
       <div>
         <ul *ngFor="let item of expArrToDo">
           <li *ngIf="item.completed === false" class="py-1 fs-5 mt-3 d-flex justify-content-between w-50 mx-auto bg-light rounded-pill border border-dark">
-            <span *ngIf="item.modificated === false" class="align-self-center ps-4 fw-bold">{{ item.title }}</span>
-            <input type="text" *ngIf="item.modificated === true" [(ngModel)]="item.title" class="rounded-pill ps-3 ms-1" />
+            <span *ngIf="item.modificated === false" class="text-capitalize align-self-center ps-4 fw-bold">{{ item.title }}</span>
+            <input type="text" *ngIf="item.modificated === true" [(ngModel)]="item.title" class="text-capitalize rounded-pill ps-3 ms-1" />
             <div class="me-2">
               <button class="btn btn-md btn-warning rounded-pill w-50 fw-bold pe-5 border" (click)="item.modificated = true" *ngIf="item.modificated === false">
                 Modifica
@@ -27,7 +27,7 @@ import { aggTask, riceviTask, arrayToDo, ricerca1, addiz, sottr } from "../servi
             </div>
           </li>
         </ul>
-        <p *ngIf="cerca === 0" class="fs-3 mt-3">{{ ricerca }}</p>
+        <p *ngIf="cerca === 0" class="text-capitalize fs-3 mt-3">{{ ricerca }}</p>
       </div>
     </div>
   `,
